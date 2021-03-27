0ffcbe8fd05ea682e364951298983e24d321c816
Eimantas Blazevicius <eimantas.blazevicius@bazaarvoice.com>
2021-03-27 09:11:50 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 import "./App.css";
-import Selector from "./components/selector";
-import AnswerField from "./components/answerField";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Container from "react-bootstrap/Container";
 
 function App() {
-  const [answer, setAnswer] = useState();
-
-  function handleSelect(selected, category) {
-    axios("http://localhost:8000/api/answers/" + selected).then(function (
-      response
-    ) {
-      setAnswer(response.data);
-      // handle success
-    });
-  }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Jumbotron fluid>
-          <Container>
-            <p>, that is the question:</p>
-            <i className="float-right">(from Hamlet, spoken by Hamlet)</i>
-            <br />
-            <i className="float-right">Maybe</i>
-          </Container>
-        </Jumbotron>
-      </header>
-      <div className="container">
-        <Selector onSelect={handleSelect} />
-        <AnswerField answer={answer} />
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
