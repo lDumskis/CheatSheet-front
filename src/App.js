a36dc64bc1ef55982f94e44e6b652e83ce0d0ba7
ldumskis <lukas.dumskis@bazaarvoice.com>
2021-03-27 16:09:39 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
 import React, { useState } from "react";
-import axios from "axios";
 import "./App.css";
-import Selector from "./components/selector";
-import AnswerField from "./components/answerField";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Container from "react-bootstrap/Container";
+import Header from "./components/header";
+
 
 function App() {
-  const [answer, setAnswer] = useState();
-
-  function handleSelect(selected, category) {
-    axios("http://localhost:8000/api/answers/" + selected).then(function (
-      response
-    ) {
-      setAnswer(response.data);
-      // handle success
-    });
-  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <Jumbotron fluid>
-          <Container>
-            <p>, that is the question:</p>
-            <i className="float-right">(from Hamlet, spoken by Hamlet)</i>
-            <br />
-            <i className="float-right">Maybe</i>
-          </Container>
-        </Jumbotron>
-      </header>
-      <div className="container">
-        <Selector onSelect={handleSelect} />
-        <AnswerField answer={answer} />
-      </div>
-    </div>
+    <div className="container-fluid"><Header></Header></div>
   );
 }
 
